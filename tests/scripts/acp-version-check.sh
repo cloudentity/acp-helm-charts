780575eb01101ed7513b6685c530113bc5c6fd88
Krzysztof Lisocki <klisocki@cloudentity.com>
2022-02-16 09:51:45 +0100

    [AUT-5288] Synced ACP chart versions

--- tests/scripts/acp-version-check.sh
+++ tests/scripts/acp-version-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ACP_VERSION="$(yq eval '(.dependencies[]|select(.name == "acp").version)' ./charts/kube-acp-stack/Chart.yaml)"
-ACP_VERSION_IN_KUBE_ACP_STACK="$(yq eval '(.version)' ./charts/acp/Chart.yaml)"
+ACP_VERSION="$(yq eval '(.version)' ./charts/acp/Chart.yaml)"
+ACP_VERSION_IN_KUBE_ACP_STACK="$(yq eval '(.dependencies[]|select(.name == "acp").version)' ./charts/kube-acp-stack/Chart.yaml)"
 echo "'acp' chart version is [$ACP_VERSION]"
 echo "'acp' chart version in 'kube-acp-stack' chart is [$ACP_VERSION_IN_KUBE_ACP_STACK]"
 if [ "$ACP_VERSION" == "$ACP_VERSION_IN_KUBE_ACP_STACK" ]
