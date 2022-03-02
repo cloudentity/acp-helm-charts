71ed2af4755434b6d4353d119ba112a58226c6b2
Lukasz Gonczarik <17554958+lgonczarik@users.noreply.github.com>
2022-03-02 12:10:22 +0100

    Added update extension config with yq

--- tests/scripts/update-istio-configmap.sh
+++ tests/scripts/update-istio-configmap.sh
@@ -2,12 +2,24 @@
 
 set -e
 
-ISTIO_NAMESPACE="istio-system"
-WORKDIR=tests/config/tmp
-mkdir -p $WORKDIR
+ISTIO_AUTH_APP="${ISTIO_AUTH_APP:-istio-authoriszer}"
+ISTIO_AUTH_APP_NAMESPACE="${ISTIO_NAMESPACE:-istio-authorizer}"
+ISTIO_NAMESPACE="${ISTIO_NAMESPACE:-istio-system}"
 
+EXTENSION_CONFIG_LOCATION="tests/config/istio-extension-providers-config.yaml"
+WORKDIR="tests/config/tmp"
+
+# Prepare configuration
+yq eval --inplace ".extensionProviders.[] |= select(.name == \"acp-authorizer\").envoyExtAuthzGrpc.service |= \"$ISTIO_AUTH_APP.$ISTIO_AUTH_APP_NAMESPACE.svc.cluster.local\"" $EXTENSION_CONFIG_LOCATION
+
+# Fetch current configmap
+mkdir -p $WORKDIR
 kubectl get configmap istio -n $ISTIO_NAMESPACE -o yaml >$WORKDIR/istio-configmap.yaml
-mesh_config=$(yq eval-all '.data.mesh' $WORKDIR/istio-configmap.yaml | yq eval-all '. as $item ireduce ({}; . * $item )' - tests/config/istio-extension-providers-config.yaml)
+
+# Update configmap
+mesh_config=$(yq eval-all '.data.mesh' $WORKDIR/istio-configmap.yaml | yq eval-all '. as $item ireduce ({}; . * $item )' - $EXTENSION_CONFIG_LOCATION)
 data=$mesh_config yq eval-all ".data.mesh = strenv(data)" $WORKDIR/istio-configmap.yaml >$WORKDIR/istio-configmap-new.yaml
 kubectl apply -f $WORKDIR/istio-configmap-new.yaml
+
+# Apply changes to istio
 kubectl rollout restart deployment/istiod -n $ISTIO_NAMESPACE
