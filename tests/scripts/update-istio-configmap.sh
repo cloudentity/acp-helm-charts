01f87635729a3b87426a568fbbce1ca8957e323f
Lukasz Gonczarik <17554958+lgonczarik@users.noreply.github.com>
2022-03-02 09:26:38 +0100

    Added itio restart to script

--- tests/scripts/update-istio-configmap.sh
+++ tests/scripts/update-istio-configmap.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+ISTIO_NAMESPACE="istio-system"
 WORKDIR=tests/config/tmp
 mkdir -p $WORKDIR
 
-kubectl get configmap istio -n istio-system -o yaml >$WORKDIR/istio-configmap.yaml
+kubectl get configmap istio -n $ISTIO_NAMESPACE -o yaml >$WORKDIR/istio-configmap.yaml
 mesh_config=$(yq eval-all '.data.mesh' $WORKDIR/istio-configmap.yaml | yq eval-all '. as $item ireduce ({}; . * $item )' - tests/config/istio-extension-providers-config.yaml)
 data=$mesh_config yq eval-all ".data.mesh = strenv(data)" $WORKDIR/istio-configmap.yaml > $WORKDIR/istio-configmap-new.yaml
 kubectl apply -f $WORKDIR/istio-configmap-new.yaml
+kubectl rollout restart deployment/istiod -n $ISTIO_NAMESPACE
