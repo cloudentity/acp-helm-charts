818b5ed6b5b7dfc0aaac4a89a6715871f521fea6
David Joo <djoo@cloudentity.com>
2022-04-18 11:39:23 -0700

    AUT-5740 Bump acp app version only

--- tests/scripts/bump-acp-version.sh
+++ tests/scripts/bump-acp-version.sh
@@ -8,10 +8,8 @@ ACP_VERSION=$2
 for CHART in ${HELM_CHARTS//,/ }
 do
   if [[ "${CHART}" == "kube-acp-stack" ]]; then
-    yq eval -i '.version = strenv(ACP_VERSION)' ./charts/${CHART}/Chart.yaml
     yq eval -i '.dependencies[] | select(.name == "acp").version = strenv(ACP_VERSION)' ./charts/${CHART}/Chart.yaml
   else
-    yq eval -i '.version = strenv(ACP_VERSION)' ./charts/${CHART}/Chart.yaml
     yq eval -i '.appVersion = strenv(ACP_VERSION)' ./charts/${CHART}/Chart.yaml
   fi
 done
